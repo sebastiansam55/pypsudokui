6bbac0cd8daf55cd69c917fd345e2ce3ae0b4947
Sam Sebastian <sebastiansam55@gmail.com>
2021-03-23 02:12:40 -0400

    Update fetch script

--- pifetch.py
+++ pifetch.py
@@ -5,8 +5,8 @@ import json
 
 url = "https://api.pi.delivery/v1/pi?start={}&numberOfDigits=1000"
 
-with open("pydigits.json", "w") as f:
-    for i in range(0, 10000):
+with open("pydigits_long.json", "w") as f:
+    for i in range(0, 1000000):
         print(url.format(i*1000))
         r = requests.get(url.format(i*1000))
         f.write(json.dumps(r.json()))
